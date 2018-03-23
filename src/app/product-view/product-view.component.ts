189f5972b3b6f808e20d134ae6b42d570ea61170
stantly001 <stantly001@gmail.com>
2018-03-23 11:43:52 +0530

    zoom library included

--- src/app/product-view/product-view.component.ts
+++ src/app/product-view/product-view.component.ts
@@ -1,25 +1,33 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { DefaultService } from '../services/default.service';
+import * as $ from 'jquery';
+import 'jquery-zoom';
+declare var jQuery: any;
 @Component({
   selector: 'app-product-view',
   templateUrl: './product-view.component.html',
   styleUrls: ['./product-view.component.css']
 })
+
 export class ProductViewComponent implements OnInit {
   isOn: boolean;
   productId: number;
   productsArr: any;
   current: number = 0;
   items: Array<any>;
-  @ViewChild('videoPlayer') videoplayer: any;
-
+  @ViewChild('videoPlayer') videoplayer: any;  
 
   constructor(private activatedRoute: ActivatedRoute, private defaultService: DefaultService) {
     this.productId = parseInt(this.activatedRoute.snapshot.params["productId"]);
   }
 
   ngOnInit() {
+    $(document).ready(function(){
+			(<any>$('#ex1')).zoom();
+		
+		});
+    
     this.isOn=false;
     this.defaultService.getProducts().subscribe(response => {
       this.productsArr = response.filter(product => product.id === this.productId)[0];
